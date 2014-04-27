174ba16a270eaba44f68afd90d53935e7d0c7132
Gary Burgess <gary.burgess@gmail.com>
2014-04-27 11:35:16 +0100

    Derive maybe instances

--- src/Test/QuickCheck/Maybe.purs
+++ src/Test/QuickCheck/Maybe.purs
@@ -1,4 +1,4 @@
-module Test.QuickCheck.Maybe where
+module Test.QuickCheck.Maybe (TestMaybe(..), runTestMaybe) where
 
 import Data.Maybe
 
@@ -9,7 +9,38 @@ data TestMaybe a = TestMaybe (Maybe a)
 runTestMaybe :: forall a. TestMaybe a -> Maybe a
 runTestMaybe (TestMaybe m) = m
 
+lift :: forall a b. (Maybe a -> Maybe a -> b) -> TestMaybe a -> TestMaybe a -> b
+lift f (TestMaybe x) (TestMaybe y) = f x y
+
 instance arbMaybe :: (Arbitrary a) => Arbitrary (TestMaybe a) where
   arbitrary = TestMaybe <$> do
     b <- arbitrary
     if b then pure Nothing else Just <$> arbitrary
+    
+instance showTestMaybe :: (Show a, Show b) => Show (TestMaybe a) where
+  show (TestMaybe x) = "TestMaybe " ++ show x
+    
+instance eqTestMaybe :: (Eq a) => Eq (TestMaybe a) where
+  (==) = lift (==)
+  (/=) = lift (/=)
+  
+instance ordTestMaybe :: (Ord a) => Ord (TestMaybe a) where
+  compare = lift compare
+
+instance functorTestMaybe :: Functor TestMaybe where
+  (<$>) f (TestMaybe x) = TestMaybe $ f <$> x
+  
+instance applyTestMaybe :: Apply TestMaybe where
+  (<*>) (TestMaybe f) (TestMaybe x) = TestMaybe $ f <*> x
+  
+instance applicativeTestMaybe :: Applicative TestMaybe where
+  pure = TestMaybe <<< pure
+  
+instance alternativeTestMaybe :: Alternative TestMaybe where
+  empty = TestMaybe empty
+  (<|>) (TestMaybe l) (TestMaybe r) = TestMaybe (l <|> r)
+  
+instance bindTestMaybe :: Bind TestMaybe where
+  (>>=) (TestMaybe x) f = TestMaybe (x >>= runTestMaybe <<< f)
+
+instance monadTestMaybe :: Monad TestMaybe
