931db949188f529ec81f8432f50e6c0f0fa1172d
Gary Burgess <gary.burgess@gmail.com>
2014-04-27 11:35:25 +0100

    Derive tuple instances

--- src/Test/QuickCheck/Tuple.purs
+++ src/Test/QuickCheck/Tuple.purs
@@ -1,6 +1,7 @@
-module Test.QuickCheck.Tuple where
+module Test.QuickCheck.Tuple (TestTuple(..), runTestTuple) where
 
 import Data.Tuple
+import Data.Monoid
 
 import Test.QuickCheck
 
@@ -9,5 +10,32 @@ data TestTuple a b = TestTuple (Tuple a b)
 runTestTuple :: forall a b. TestTuple a b -> Tuple a b
 runTestTuple (TestTuple t) = t
 
+lift :: forall a b c. (Tuple a b -> Tuple a b -> c) -> TestTuple a b -> TestTuple a b -> c
+lift f (TestTuple x) (TestTuple y) = f x y
+
 instance arbTuple :: (Arbitrary a, Arbitrary b) => Arbitrary (TestTuple a b) where
-arbitrary = TestTuple <$> (Tuple <$> arbitrary <*> arbitrary)
+  arbitrary = TestTuple <$> (Tuple <$> arbitrary <*> arbitrary)
+    
+instance showTestTuple :: (Show a, Show b) => Show (TestTuple a b) where
+  show (TestTuple x) = "TestTuple " ++ show x
+    
+instance eqTestTuple :: (Eq a, Eq b) => Eq (TestTuple a b) where
+  (==) = lift (==)
+  (/=) = lift (/=)
+  
+instance ordTestTuple :: (Ord a, Ord b) => Ord (TestTuple a b) where
+  compare = lift compare
+
+instance functorTestTuple :: Functor (TestTuple a) where
+  (<$>) f (TestTuple x) = TestTuple $ f <$> x
+  
+instance applyTestTuple :: (Semigroup a) => Apply (TestTuple a) where
+  (<*>) (TestTuple f) (TestTuple x) = TestTuple $ f <*> x
+  
+instance applicativeTestTuple :: (Monoid a) => Applicative (TestTuple a) where
+  pure = TestTuple <<< pure
+  
+instance bindTestTuple :: (Semigroup a) => Bind (TestTuple a) where
+  (>>=) (TestTuple x) f = TestTuple (x >>= runTestTuple <<< f)
+
+instance monadTestTuple :: (Monoid a) => Monad (TestTuple a)
